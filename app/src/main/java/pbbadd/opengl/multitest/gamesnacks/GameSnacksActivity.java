56571aee92dec041ac4a88520225d70cf45ede5e
AMPBB <1942692994@qq.com>
2026-07-17 21:55:57 +0800

    change gamesnacks activity
    modify load real url not static html
    webview.load(<real_url>)

--- app/src/main/java/pbbadd/opengl/multitest/gamesnacks/GameSnacksActivity.java
+++ app/src/main/java/pbbadd/opengl/multitest/gamesnacks/GameSnacksActivity.java
@@ -7,12 +7,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
@@ -20,25 +22,14 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 import pbbadd.opengl.multitest.R;
 
 public class GameSnacksActivity extends AppCompatActivity {
     private static final String TAG = "GameSnacksActivity";
-    private static final String ASSET_HTML = "GameSnacks_Personalization_settings.html";
-    private static final String BASE_URL = "https://gamesnacks.com/";
+    private static final String GAME_SNACKS_URL = "https://gamesnacks.com/";
     private static final boolean ENABLE_GPU_COMPOSITION_STRESS = true;
     private static final int GPU_COMPOSITION_STRESS_LAYER_COUNT = 1;
 
@@ -47,22 +38,15 @@ public class GameSnacksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        gameMode();
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game_snacks);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         gameSnacksWebView = findViewById(R.id.gamesnacks_webview);
         gpuCompositionStressContainer = findViewById(R.id.gamesnacks_gpu_composition_stress_container);
         initWebView();
         initGpuCompositionStressLayers();
-        loadGameSnacksHtml();
+        loadGameSnacksUrl();
     }
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -87,10 +71,6 @@ public class GameSnacksActivity extends AppCompatActivity {
         settings.setMediaPlaybackRequiresUserGesture(false);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
         settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         settings.setCacheMode(WebSettings.LOAD_DEFAULT);
         settings.setOffscreenPreRaster(true);
@@ -117,33 +97,24 @@ public class GameSnacksActivity extends AppCompatActivity {
         return (int) (dp * getResources().getDisplayMetrics().density + 0.5f);
     }
 
-    private void loadGameSnacksHtml() {
-        try {
-            String html = readAssetText(ASSET_HTML);
-            gameSnacksWebView.loadDataWithBaseURL(BASE_URL, html, "text/html", "UTF-8", BASE_URL);
-        } catch (IOException e) {
-            Log.e(TAG, "load asset failed", e);
-            Toast.makeText(this, "load html failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private String readAssetText(String assetName) throws IOException {
-        try (InputStream inputStream = getAssets().open(assetName);
-             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[16 * 1024];
-            int read;
-            while ((read = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, read);
-            }
-            return outputStream.toString(StandardCharsets.UTF_8.name());
-        }
+    private void loadGameSnacksUrl() {
+        gameSnacksWebView.loadUrl(GAME_SNACKS_URL);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        applyImmersiveMode();
         gameSnacksWebView.onResume();
         gameSnacksWebView.resumeTimers();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyImmersiveMode();
+        }
     }
 
     @Override
@@ -157,6 +128,34 @@ public class GameSnacksActivity extends AppCompatActivity {
     protected void onDestroy() {
         gameSnacksWebView.destroy();
         super.onDestroy();
+    }
+
+    private void gameMode() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+        );
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        applyImmersiveMode();
+    }
+
+    private void applyImmersiveMode() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
     }
 
     private static class CompositionStressSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
